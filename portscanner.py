a225ff05b4c3656eb025bbe0f1f3f37d57d90368
wevelll <wevel@LAPTOP-ACJ1H5PR.localdomain>
2019-09-12 17:24:18 +0300

    added some features

--- portscanner.py
+++ portscanner.py
@@ -2,17 +2,18 @@ import socket
 import sys
 
 
-def scan_ports(addr):
+
+def scan_ports(host, ports):
     print ('scanning...')
-    for port in range(1, 1024):
+    for port in ports.keys():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(0.5)
         try:
-            result = s.connect_ex((addr, port))
+            result = s.connect_ex((host, port))
             if result == 0:
-                print ('port {}: open'.format(port))
+                print ('[+]port {0} {1}: open'.format(port, ports[port]))
             else:
-                print ('port {}: closed'.format(port))
+                print ('[-]port {0} {1}: closed'.format(port, ports[port]))
             s.close()
         except socket.gaierror:
             print ('hostname not resolved')
@@ -23,14 +24,18 @@ def scan_ports(addr):
 
 
 def main():
+    ports = {20: 'FTP', 21: 'FTP', 22: 'SSH', 23: 'telnet', 25: 'SMTP',  53: 'DNS', 80: 'HTTP', 110: 'POP3', 143: 'IMAP', 443: 'HTTPS', 587: 'SMTP', 993: 'IMAP-SSL', 995: 'POP3S', 2083: 'cPanel(user)', 2087: 'cPanel(admin)', 2222: 'DirectAdmin',3128: 'HTTP-PROXY', 3306: 'MySQL', 8080: 'HTTP-PROXY', 8083: 'Vesta'}
     if (len(sys.argv) < 2):
         host = input('No cl args, input hostname: ')
-        scan_ports(host)
+        scan_ports(host, ports)
     else:
         hosts = sys.argv
         hosts.remove(sys.argv[0])
+        if '-a' in sys.argv:
+            ports = range(1, 1024)
+            hosts.remove('-a')
         for host in hosts:
-            scan_ports(host)
+            scan_ports(host, ports)
 
 
 if __name__ == '__main__':
