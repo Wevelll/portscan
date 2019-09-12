5b34c53df4116114be1810e077e0f032d8a411ed
wevelll <wevel@LAPTOP-ACJ1H5PR.localdomain>
2019-09-12 11:44:25 +0300

    quie working

--- portscanner.py
+++ portscanner.py
@@ -2,24 +2,34 @@ import socket
 import sys
 
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def scan_ports(addr):
+    print ('scanning...')
     try:
         for port in range(1, 16535):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.01)
             result = s.connect_ex((addr, port))
             if result == 0:
                 print ('port {}: open'.format(port))
+            s.close()
+    except socket.gaierror:
+        print ("hostname not resolved")
+        return
+    except socket.error:
+        print ("couldn't connect")
+        return
+
 
 def main():
     if (len(sys.argv) < 2):
-        host = raw_input('No cl args, input hostname: ')
-        scan_ports(socket.gethostbyname(host))
+        host = input('No cl args, input hostname: ')
+        scan_ports(host)
     else:
         hosts = sys.argv
         hosts.remove(sys.argv[0])
         for host in hosts:
-            scan_ports(socket.gethostbyname(host))
+            scan_ports(host)
+
 
 if __name__ == '__main__':
     main()
